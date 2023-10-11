8ad60c671f19070c3ca9b4695e2e7f57c504f74f
Gomah <hi@gomah.fr>
2023-10-12 09:14:22 +1100

    chore!: drop node 14 & bump deps

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { utm } from '../src';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 module.exports = async (req: VercelRequest, res: VercelResponse): Promise<void> => {
-  const query = req.url.replace('/', '');
+  const query = req?.url?.replace('/', '');
 
   const utms = utm(query);
 
