310dc1343eb2d819eb551016ddc553149d8ba4ff
Gomah <hi@gomah.fr>
2023-12-11 09:46:53 +1100

    feat: ssr friendly

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import qs from 'query-string';
 type UtmParams = {
   utm_source?: string;
   utm_medium?: string;
+  utm_campaign?: string;
   utm_term?: string;
   utm_content?: string;
   utm_name?: string;
   [key: string]: string | string[] | undefined | null | boolean | number;
 };
 
-export function utm(query: string = window?.location?.search): UtmParams {
+export function utm(
+  query: string = typeof window !== 'undefined' ? window?.location?.search : ''
+): UtmParams {
   const utms = qs.pick(query, (name) => name.startsWith('utm_'));
   return qs.parse(utms) as UtmParams;
 }
